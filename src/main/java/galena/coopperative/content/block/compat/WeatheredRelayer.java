5e5421df1038b82a1af731bdbb27f9946d826736
Niklas Widmann <nik@somethingcatchy.net>
2023-06-01 08:45:35 +0200

    sort correctly in creative tabs

--- src/main/java/galena/coopperative/content/block/compat/WeatheredRelayer.java
+++ src/main/java/galena/coopperative/content/block/compat/WeatheredRelayer.java
@@ -2,8 +2,13 @@ package galena.coopperative.content.block.compat;
 
 import galena.coopperative.content.block.CWeatheringCopper;
 import net.mehvahdjukaar.supplementaries.common.block.blocks.RelayerBlock;
+import net.mehvahdjukaar.supplementaries.reg.ModRegistry;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.jetbrains.annotations.NotNull;
 
 public class WeatheredRelayer extends RelayerBlock implements CWeatheringCopper {
 
@@ -17,6 +22,11 @@ public class WeatheredRelayer extends RelayerBlock implements CWeatheringCopper 
     @Override
     public WeatherState getAge() {
         return weatherState;
+    }
+
+    @Override
+    public void fillItemCategory(@NotNull CreativeModeTab tab, @NotNull NonNullList<ItemStack> items) {
+        insert(this, false, items, itemStack -> itemStack.is(ModRegistry.RELAYER.get().asItem()), true);
     }
 
 }
