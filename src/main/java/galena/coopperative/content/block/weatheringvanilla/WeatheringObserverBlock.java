c1be35780d4ab4d088ebce9025beb7181bc38442
Niklas Widmann <nik@somethingcatchy.net>
2023-06-01 08:46:05 +0200

    fix observers connecting to redstone on wrong sides

--- src/main/java/galena/coopperative/content/block/weatheringvanilla/WeatheringObserverBlock.java
+++ src/main/java/galena/coopperative/content/block/weatheringvanilla/WeatheringObserverBlock.java
@@ -3,17 +3,19 @@ package galena.coopperative.content.block.weatheringvanilla;
 import galena.coopperative.content.block.CWeatheringCopper;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ObserverBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Optional;
-import java.util.Random;
+import org.jetbrains.annotations.Nullable;
 
 @MethodsReturnNonnullByDefault
 public class WeatheringObserverBlock extends ObserverBlock implements CWeatheringCopper {
@@ -25,16 +27,6 @@ public class WeatheringObserverBlock extends ObserverBlock implements CWeatherin
         this.weatherState = weatherState;
     }
 
-    /*public void randomTick(BlockState state, ServerLevel world, BlockPos pos, Random random) {
-        this.onRandomTick(state, world, pos, random);
-        super.randomTick(state, world, pos, random);
-    }
-
-    @Override
-    public boolean isRandomlyTicking(BlockState state) {
-        return Optional.ofNullable(NEXT_BY_BLOCK.get().get(state.getBlock())).isPresent() || super.isRandomlyTicking;
-    }*/
-
     @Override
     public WeatherState getAge() {
         return this.weatherState;
@@ -43,5 +35,11 @@ public class WeatheringObserverBlock extends ObserverBlock implements CWeatherin
     @Override
     public void fillItemCategory(@NotNull CreativeModeTab tab, @NotNull NonNullList<ItemStack> items) {
         insert(this, false, items, itemStack -> itemStack.getItem().equals(Items.OBSERVER), true);
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
+        var facing = state.getValue(FACING);
+        return direction == facing;
     }
 }
